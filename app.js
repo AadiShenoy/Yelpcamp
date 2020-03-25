251b170a9d62938efca23756c06e4d5c29d5a212
Aadi Shenoy <adithyashenoy6@gmail.com>
2020-03-25 13:13:14 +0000

    portchanged300

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function(req, res, next){
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-app.listen(3000, function() { 
-  console.log('yelpcamp dtabase has started'); 
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
 
